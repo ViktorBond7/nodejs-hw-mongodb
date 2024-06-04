53b706d16bf87851c26ac7570319e4c5c831cd2c
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2024-06-04 21:24:24 +0200

    error id

--- src/middlewares/notFoundMiddleware.js
+++ src/middlewares/notFoundMiddleware.js
@@ -1,5 +1,9 @@
 const notFoundtMiddleware = (reg, res, next) => {
-  res.status(404).json({ message: 'Not found' });
+  res.status(404).json({
+    status: 404,
+    message: 'Not found',
+    data: [],
+  });
 };
 
 export default notFoundtMiddleware;
