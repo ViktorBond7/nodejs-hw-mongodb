01aff710b383677d108d7d2b3e0b58dd39f67d19
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2024-06-11 10:50:18 +0200

    sort

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,14 @@
 import createHttpError from 'http-errors';
 import { ContactColection } from '../db/models/contacts.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
+import { SORT_ORDER } from '../contacts/index.js';
 
-export const getAllContacts = async ({ page, perPage }) => {
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+}) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
@@ -11,7 +17,12 @@ export const getAllContacts = async ({ page, perPage }) => {
     .merge(contactsQuery)
     .countDocuments();
 
-  const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder === 'desc' ? -1 : 1 })
+    // .sort({ [sortBy]: sortOrder })
+    .exec();
 
   const paginationData = calculatePaginationData(contactsCount, perPage, page);
 
